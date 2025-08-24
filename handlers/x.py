12e264b362305b1de459e9f14ac9150442c748a1
VersaNexusIX <besoklibur457@gmail.com>
2025-08-24 13:01:28 +0700

    Update x.py

--- handlers/x.py
+++ handlers/x.py
@@ -4,12 +4,12 @@ from colorama import Fore, Style
 
 def handle(args):
     if not args:
-        return Fore.YELLOW + "⚠️ Link X/Twitter kosong." + Style.RESET_ALL
+        return Fore.YELLOW + "⚠️ empty link" + Style.RESET_ALL
 
     url = args[0]
     choice = ask_format()
     if choice == "4":
-        return Fore.CYAN + "❎ Dibatalkan oleh pengguna." + Style.RESET_ALL
+        return Fore.CYAN + "❎ cancelled by user" + Style.RESET_ALL
 
     ext = get_ext(choice)
     filename = get_next_filename(ext, prefix="X_")   # untuk x.py
@@ -22,15 +22,15 @@ def handle(args):
                 f.write(chunk)
 
         final_path = movetopublicandscan(filename)
-        return Fore.GREEN + f"✅ X: Disimpan di: {final_path}" + Style.RESET_ALL
+        return Fore.GREEN + f"✅ X: file saved to: {final_path}" + Style.RESET_ALL
 
     except Exception as e:
-        return Fore.RED + f"❌ Gagal unduh X: {e}" + Style.RESET_ALL
+        return Fore.RED + f"❌ Failed to download: {e}" + Style.RESET_ALL
 
 def ask_format():
-    print(Fore.CYAN + "\nPilih format unduhan:")
+    print(Fore.CYAN + "\nChoose Format:")
     print("1. Mp3\n2. Mp4\n3. Jpg\n4. Cancel" + Style.RESET_ALL)
-    return input("Pilihan (1/2/3/4): ").strip()
+    return input("Choose (1/2/3/4): ").strip()
 
 def get_ext(choice):
     return {"1": "mp3", "2": "mp4", "3": "jpg"}.get(choice, "bin")
